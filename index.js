70cd4da5ea0c57503fef84810195bb3eb15b0500
Filip Sojecki <faunbox2@gmail.com>
2020-12-11 15:41:52 +0100

    updating gitignore

--- index.js
+++ index.js
@@ -5,27 +5,26 @@ const Animal = function (name, spieces, age = "niewiadomo ile") {
   this.name = name;
   this.spieces = spieces;
   Animal.prototype.click = function () {
-    if (name != "Puszek") {
-      console.log(
-        `Zwierze ma na imie ${name}, ma ${age} lat i jest rasy ${spieces}`
-      );
-    } else {
-      console.table(`${name}, ${spieces}`);
-    }
+    console.log(
+      `Zwierze ma na imie ${name}, ma ${age} lat i jest rasy ${spieces}`
+    );
   };
 };
 
 class Animal2 extends Animal {
   constructor(name, age, spieces) {
-    super(name, age, spieces);
-    // this.name = name;
-    // this.age = age;
-    // this.spieces = spieces;
+    super(name, spieces, age);
+    this.name = name;
+    this.age = age;
+    this.spieces = spieces;
+  }
+  click2() {
+    return this.click();
   }
 }
 
 const cat = new Animal("Koko", "Kotek Brytyjski");
 const dog = new Animal2("Puszek", "10", "mieszaniec");
 
-btn.addEventListener("click", cat.click);
-btn2.addEventListener("click", dog.click);
+btn.addEventListener("click", dog.click);
+btn2.addEventListener("click", dog.click2);
